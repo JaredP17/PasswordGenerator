8606e4011474371c93e9a7c2f3e5eb14dc57cb37
Jared Porter <jaredp17@me.com>
2020-06-06 18:43:50 -0500

    Added new variables for boolean checks and single char string to be built. Also included conditional statements to add character types.

--- Develop/script.js
+++ Develop/script.js
@@ -13,13 +13,15 @@ function writePassword() {
 // Generate random password function
 function generatePassword() {
   var isGenerated = false;
-  var passwordLength;
+  var passwordLength = 0;
   var passwd = "";
-  var charSelction = "";
+  var charStr = ""; // Character string to be used for password generation
   var lower = "abcdefghijklmnopqrstuvwxyz";
   var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var nums = "0123456789";
   var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  var charTypeSelected = false;
+  var charTypeLog = "Input validated!\nPassword will be generated with these character types:\n\n";
 
   while (!isGenerated) {
     // Prompt user for password length. Default value set to minimum number of characters allowed.
@@ -35,14 +37,48 @@ function generatePassword() {
       alert("You must choose a password length of at least 8 characters and no more than 128 characters. Try again!");
     }
     else { // Valid input, proceed.
-      alert("Valid input");
-      passwd = "Test Password"
-      return passwd;
-      break;
+      // Prompt user for character types to be used in password.
+      while (!charTypeSelected) {
+        // Ask to include lowercase letters
+        if (confirm("Include lowercase letters?")) {
+          charStr += lower;
+          charTypeLog += "Lowercase\n";
+          charTypeSelected = true;
+        }
+        // Ask to include uppercase letters
+        if (confirm("Include uppercase letters?")) {
+          charStr += upper;
+          charTypeLog += "Uppercase\n";
+          charTypeSelected = true;
+        }
+        // Ask to include numbers
+        if (confirm("Include numbers?")) {
+          charStr += nums;
+          charTypeLog += "Numbers\n";
+          charTypeSelected = true;
+        }
+        // Ask to include special characters
+        if (confirm("Include special characters?")) {
+          charStr += specialChars;
+          charTypeLog += "Special Characters\n";
+          charTypeSelected = true;
+        }
+
+        // Check if character type has been selected
+        if (charTypeSelected) {
+          console.log(charTypeLog +"\n");
+          console.log(charStr + "\n");
+        } else {
+          alert("At least one character type should be selected!");
+        }   
+      }
+
+      isGenerated = true;
     }
   }
 
-
+  passwd = "Test Password"
+  return passwd;
 }
 
 // Add event listener to generate button
